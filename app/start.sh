e3d74dce9e482cad2a7a6ac0418b833b223bd08a
Roel Beerens <roel@sumedia.nl>
2018-03-29 23:22:34 +0200

    Test

--- app/start.sh
+++ app/start.sh
@@ -5,11 +5,11 @@ export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
 # Is there an active WiFi connection?
 SSID=`iwgetid -r`
 
-if [ "$SSID" ]; then
+if [ ! "$SSID" ]; then
     printf 'Already connected. Skipping WiFi Connect\n'
+    ./wifi-connect --portal-ssid "Hound" --ui-directory "lib/wificonnect/ui"
 else
     printf 'Not connected. Starting WiFi Connect\n'
-    ./wifi-connect --portal-ssid "Hound" --ui-directory "lib/wificonnect/ui"
 fi
 
 sleep 1
