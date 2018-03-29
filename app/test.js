6a0e28f5abe5dade57071a79d789a20a547fbf01
Roel Beerens <roel@sumedia.nl>
2018-03-29 21:36:28 +0200

    Testing

---
+++ app/test.js
@@ -0,0 +1,9 @@
+const exec = require('child_process').exec;
+
+exec('nmcli -t g | grep full', (error) => {
+    'use strict';
+    if (error) {
+        console.log(chalk.red(`exec error: ${error}`));
+        return;
+    }
+});
