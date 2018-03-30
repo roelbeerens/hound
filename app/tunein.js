93183a5a6446077db1f6d306c47300624c5e9d86
Roel Beerens <roel@sumedia.nl>
2018-03-30 23:58:09 +0200

    mpv test

--- app/tunein.js
+++ app/tunein.js
@@ -4,16 +4,21 @@ const tunein = new TuneIn({
     cacheRequests: false,            // Wheter or not to cache requests, default false
     cacheTTL: 1000 * 60 * 5,    // TTL for cached results, default 5 mins
 });
-var MPlayer = require('mplayer');
-var player = new MPlayer();
+let mpv = require('node-mpv');
+let mpvPlayer = new mpv();
 
 tunein.browse_local().then(function (result) {
     let stations = result.body[0].children;
     let radio = stations[3];
     tunein.tune_radio(radio.guide_id).then(function (result) {
         let stream = result.body[0];
-        player.openFile(stream.url);
+        mpvPlayer.load(stream.url);
+        mpvPlayer.volume(50);
     });
 }).catch(function (err) {
     console.log(err);
+});
+
+mpvPlayer.on('statuschange', function(status){
+    console.log(status);
 });
