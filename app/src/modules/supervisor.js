5b62bbe843414d5230ecfa0b08581e23b533cdb8
Roel Beerens <roel@sumedia.nl>
2018-04-02 13:43:16 +0200

    Reboot > restart

--- app/src/modules/supervisor.js
+++ app/src/modules/supervisor.js
@@ -2,19 +2,10 @@ const axios = require('axios');
 const chalk = require('chalk');
 
 module.exports = {
-    shutdown: function () {
-        axios.post(process.env.RESIN_SUPERVISOR_ADDRESS + '/v1/shutdown?apikey=' + process.env.RESIN_SUPERVISOR_API_KEY)
-            .then(function (response) {
-                if (response.Data === 'OK') {
-                    console.log(chalk.magenta('Shutting down', process.env.RESIN_APP_NAME));
-                }
-            })
-            .catch(function (error) {
-                console.log(chalk.red(error));
-            });
-    },
     reboot: function () {
-        axios.post(process.env.RESIN_SUPERVISOR_ADDRESS + '/v1/reboot?apikey=' + process.env.RESIN_SUPERVISOR_API_KEY)
+        axios.post(process.env.RESIN_SUPERVISOR_ADDRESS + '/v1/restart?apikey=' + process.env.RESIN_SUPERVISOR_API_KEY, {
+            appId: process.env.RESIN_APP_ID
+        })
             .then(function (response) {
                 if (response.Data === 'OK') {
                     console.log(chalk.magenta('Rebooting ' + process.env.RESIN_APP_NAME));
