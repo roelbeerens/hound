e11b9449cc9f5186fc5b335abbf2e4cf131109ac
Roel Beerens <roel@sumedia.nl>
2018-04-02 16:32:13 +0200

    Permissions

--- app/src/modules/audio.js
+++ app/src/modules/audio.js
@@ -7,7 +7,7 @@ const db = low(adapter);
 
 module.exports = {
   profile: function (profile) {
-    exec(`./usr/src/app/lib/equal/set ${profile}`, (error) => {
+    exec(`/usr/src/app/lib/equal/set ${profile}`, (error) => {
       if (error) {
         console.log(chalk.red(`exec error: ${error}`));
         return;
