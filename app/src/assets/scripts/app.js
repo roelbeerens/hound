5b393d8d00402be1c988adb267fc17a214bff03f
Roel Beerens <roel@sumedia.nl>
2018-04-02 14:20:21 +0200

    Added icons

--- app/src/assets/scripts/app.js
+++ app/src/assets/scripts/app.js
@@ -1,181 +1,187 @@
 var xhr = false,
-    player = false;
+  player = false;
 
 function playerStatus() {
-    player = $.ajax({
-        url: '/player/status',
-        beforeSend: function () {
-            if (player && player.readyState !== 4) {
-                player.abort();
-            }
-        },
-    }).done(function (status) {
-        if (status.mute === true) {
-            $('.controls .mute').html('Unmute');
-        } else {
-            $('.controls .mute').html('Mute');
-        }
+  player = $.ajax({
+    url: '/player/status',
+    beforeSend: function () {
+      if (player && player.readyState !== 4) {
+        player.abort();
+      }
+    },
+  }).done(function (status) {
+    if (status.mute === true) {
+      $('.controls .mute').html('Unmute');
+    } else {
+      $('.controls .mute').html('Mute');
+    }
 
-        if (status.volume !== null) {
-            $('.slider').val(status.volume);
-        }
+    if (status.volume !== null) {
+      $('.slider').val(status.volume);
+    }
 
-        if (status["media-title"] !== null) {
-            var title = status["media-title"];
-            $('body').addClass('playing');
-            if (title.includes('mp3')) {
-                $('.now-playing span').html('Fetching Information...');
-            } else {
-                $('.now-playing span').html(title);
-            }
-        } else {
-            $('body').removeClass('playing');
-        }
-    });
+    if (status["media-title"] !== null) {
+      var title = status["media-title"];
+      $('body').addClass('playing');
+      if (title.includes('mp3')) {
+        $('.now-playing span').html('Fetching Information...');
+      } else {
+        $('.now-playing span').html(title);
+      }
+    } else {
+      $('body').removeClass('playing');
+    }
+  });
 }
 
 function stopPlayer() {
-    xhr = $.ajax({
-        url: '/player/stop',
-        beforeSend: function () {
-            if (xhr && xhr.readyState !== 4) {
-                xhr.abort();
-            }
-        },
-    }).done(function (status) {
-        if ($('body').hasClass('playing')) {
-            $('body').removeClass('playing');
-        }
-        return status;
-    });
+  xhr = $.ajax({
+    url: '/player/stop',
+    beforeSend: function () {
+      if (xhr && xhr.readyState !== 4) {
+        xhr.abort();
+      }
+    },
+  }).done(function (status) {
+    if ($('body').hasClass('playing')) {
+      $('body').removeClass('playing');
+    }
+    return status;
+  });
 }
 
 function renderStations(result) {
-    var container = $('.stations').html('');
-    $.each(result, function (i, item) {
-        if (item.item === 'station') {
+  var container = $('.stations').html('');
+  $.each(result, function (i, item) {
+    if (item.item === 'station') {
 
-            var card = '<div class="col-md-6">' +
-                '<div class="card">' +
-                '  <div class="image" style="background-image: url(' + item.image + ')"></div>' +
-                '  <div class="card-body">' +
-                '    <h5 class="card-title text-truncate">' + item.text + '</h5>' +
-                '    <a href="/radio/play/' + item.guide_id + '" class="card-link play-stream">Play</a>' +
-                '  </div>' +
-                '</div>' +
-                '</div>';
+      var card = '<div class="col-md-6">' +
+        '<div class="card">' +
+        '  <div class="image" style="background-image: url(' + item.image + ')"></div>' +
+        '  <div class="card-body">' +
+        '    <h5 class="card-title text-truncate">' + item.text + '</h5>' +
+        '    <a href="/radio/play/' + item.guide_id + '" class="card-link play-stream">Play</a>' +
+        '  </div>' +
+        '</div>' +
+        '</div>';
 
-            container.append(card);
-        }
-    });
+      container.append(card);
+    }
+  });
 }
 
 $(document).ready(function () {
 
-    if ($('.stations').length) {
-        xhr = $.ajax({
-            url: "/radio/stations",
-            beforeSend: function () {
-                if (xhr && xhr.readyState !== 4) {
-                    xhr.abort();
-                }
-            },
-        }).done(function (result) {
-            renderStations(result);
-        });
+  $(document).on('click', '.link', function (event) {
+      event.preventDefault();
+      location.href = $(event.target).attr('href');
     }
+  );
 
-    $('.stations').on('click', '.play-stream', function (e) {
-        e.preventDefault();
+  if ($('.stations').length) {
+    xhr = $.ajax({
+      url: "/radio/stations",
+      beforeSend: function () {
+        if (xhr && xhr.readyState !== 4) {
+          xhr.abort();
+        }
+      },
+    }).done(function (result) {
+      renderStations(result);
+    });
+  }
 
-        let url = $(this).attr('href');
+  $('.stations').on('click', '.play-stream', function (e) {
+    e.preventDefault();
 
-        xhr = $.ajax({
-            url: url,
-            beforeSend: function () {
-                if (xhr && xhr.readyState !== 4) {
-                    xhr.abort();
-                }
-            },
-        }).done(function () {
-            playerStatus();
-        });
+    let url = $(this).attr('href');
 
-        return false;
+    xhr = $.ajax({
+      url: url,
+      beforeSend: function () {
+        if (xhr && xhr.readyState !== 4) {
+          xhr.abort();
+        }
+      },
+    }).done(function () {
+      playerStatus();
     });
 
-    $('.volume-slider .slider').on('change', function (e) {
-        e.preventDefault();
+    return false;
+  });
 
-        let level = $(this).val();
+  $('.volume-slider .slider').on('change', function (e) {
+    e.preventDefault();
 
-        xhr = $.ajax({
-            url: '/player/volume',
-            method: 'POST',
-            data: {
-                'volume': level
-            },
-            beforeSend: function () {
-                if (xhr && xhr.readyState !== 4) {
-                    xhr.abort();
-                }
-            },
-        });
+    let level = $(this).val();
+
+    xhr = $.ajax({
+      url: '/player/volume',
+      method: 'POST',
+      data: {
+        'volume': level
+      },
+      beforeSend: function () {
+        if (xhr && xhr.readyState !== 4) {
+          xhr.abort();
+        }
+      },
+    });
+  });
+
+  $('.volume-slider .stop').on('click', function (e) {
+    e.preventDefault();
+    stopPlayer();
+    return false;
+  });
+
+  $('.volume-slider .mute').on('click', function (e) {
+    e.preventDefault();
+    var $this = $(this);
+
+    xhr = $.ajax({
+      url: '/player/mute',
+      beforeSend: function () {
+        if (xhr && xhr.readyState !== 4) {
+          xhr.abort();
+        }
+      },
+    }).done(function (status) {
+      if (status.message === 'OK' && $this.hasClass('muted')) {
+        $this.removeClass('muted');
+        $this.html('Mute');
+      } else {
+        $this.addClass('muted');
+        $this.html('Unmute');
+      }
+    });
+  });
+
+  $('.stations-search').on('submit', function (e) {
+    e.preventDefault();
+
+    let query = $(this).find('input[name="search"]').val();
+
+    xhr = $.ajax({
+      url: '/radio/stations/search',
+      method: 'POST',
+      data: {
+        'query': query,
+      },
+      beforeSend: function () {
+        if (xhr && xhr.readyState !== 4) {
+          xhr.abort();
+        }
+      },
+    }).done(function (result) {
+      renderStations(result);
     });
 
-    $('.volume-slider .stop').on('click', function (e) {
-        e.preventDefault();
-        stopPlayer();
-        return false;
-    });
+    return false;
+  });
 
-    $('.volume-slider .mute').on('click', function (e) {
-        e.preventDefault();
-        var $this = $(this);
-
-        xhr = $.ajax({
-            url: '/player/mute',
-            beforeSend: function () {
-                if (xhr && xhr.readyState !== 4) {
-                    xhr.abort();
-                }
-            },
-        }).done(function (status) {
-            if (status.message === 'OK' && $this.hasClass('muted')) {
-                $this.removeClass('muted');
-                $this.html('Mute');
-            } else {
-                $this.addClass('muted');
-                $this.html('Unmute');
-            }
-        });
-    });
-
-    $('.stations-search').on('submit', function (e) {
-        e.preventDefault();
-
-        let query = $(this).find('input[name="search"]').val();
-
-        xhr = $.ajax({
-            url: '/radio/stations/search',
-            method: 'POST',
-            data: {
-                'query': query,
-            },
-            beforeSend: function () {
-                if (xhr && xhr.readyState !== 4) {
-                    xhr.abort();
-                }
-            },
-        }).done(function (result) {
-            renderStations(result);
-        });
-
-        return false;
-    });
-
+  playerStatus();
+  setInterval(function () {
     playerStatus();
-    setInterval(function () {
-        playerStatus();
-    }, 5000);
+  }, 5000);
 });
