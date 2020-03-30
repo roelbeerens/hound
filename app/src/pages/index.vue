230e26250998ebfcb3c06e541cea570a69a84b9c
Roel Beerens <roel@gravity.nl>
2020-03-30 18:01:03 +0200

    Fixes & package updates

--- app/src/pages/index.vue
+++ app/src/pages/index.vue
@@ -7,7 +7,7 @@
                 </header>
                 <div class="content">
                     <p>Welcome to Hound 🐶. A portal for streaming your music on your custom made speakers.</p>
-                    <p>Hound currently supports Apple Airplay, Spotify Connect and TuneIn.</p>
+                    <p>Hound currently supports Spotify Connect and TuneIn.</p>
                     <p>For feature requests and/or bugs, please contact me on: <a href="mailto:rjbeerens@gmail.com">rjbeerens@gmail.com</a></p>
                     <p>Happy streaming! 🎉</p>
                 </div>
